7332db9cdf71838e25a3f0de90a6b9295ab050e9
Daniel Oliveira <oficial.danieloliveira@gmail.com>
2022-09-28 18:04:16 -0300

    fix: restore api config

--- frontend/src/Services/customAPI.ts
+++ frontend/src/Services/customAPI.ts
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 export const customAPIURL =
   process.env.NODE_ENV === 'development'
-    ? 'https://pokedex-api.ondaniel.com.br/'
-    : 'http://localhost:3000/';
+    ? 'http://localhost:3000/'
+    : 'https://pokedex-api.ondaniel.com.br/';
 
 export const customAPI = axios.create({
   baseURL: customAPIURL,
